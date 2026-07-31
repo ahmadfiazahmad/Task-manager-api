20a2c55776831b3bf125b016e49603f0d9493d4e
ahmadfiazahmad <ahmadfiazahmad975@gmail.com>
2026-07-31 19:04:09 +0500

    Stage 6: Add PUT /tasks/{task_id} endpoin

--- app/main.py
+++ app/main.py
@@ -57,3 +57,21 @@ def get_task(task_id: int):
         status_code=404,
         detail="Task not found"
     )
+
+# Defines the structure of data required to update a task.
+class TaskUpdate(BaseModel):
+    title: str
+
+# Updates the title of an existing task.
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )      
